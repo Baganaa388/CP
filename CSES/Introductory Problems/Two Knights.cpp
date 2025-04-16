0a7bbaf904d56f60f531bfbaaa3aade272cc4534
baganaa <baganaa237@gmail.com>
2025-04-16 12:57:27 +0800

    just

---
+++ CSES/Introductory Problems/Two Knights.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long int solve(int k){
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    for(int k=1; k<=n; k++){
+        cout<<solve(k)<<endl;
+    }
+    return 0;
+}
