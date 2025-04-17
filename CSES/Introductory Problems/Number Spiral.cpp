100538b1cc0ec535a766e13ab76f77453b1c0710
baganaa <baganaa237@gmail.com>
2025-04-17 17:54:14 +0800

    now

---
+++ CSES/Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#define ll long long 
+
+void solve(ll a, ll b){
+
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll a,b;
+        cin>>a>>b;
+        solve(a,b);
+    }
+    return 0;
+}
