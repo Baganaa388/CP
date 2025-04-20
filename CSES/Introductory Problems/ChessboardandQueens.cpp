f1e5dc7fba58f566c470076e530cee3c0d7b10a5
baganaa <baganaa237@gmail.com>
2025-04-20 23:49:03 +0800

    last

---
+++ CSES/Introductory Problems/ChessboardandQueens.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int solve(char (&board)[8][8]){
+    return 1;
+}
+
+int main(){
+    char board[8][8];
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            cin>>board[i][j];
+        }
+    }
+    int result;
+    result = solve(board);
+    cout<<result;
+}
